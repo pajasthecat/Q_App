fe8eb95c276c575f232fb84adeb06c463633c7ae
pajasthecat <henrik.forsberg1990@gmail.com>
2017-06-07 15:03:49 +0200

    Web api

---
+++ QApp/QApp/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QApp.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QApp.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        MilljasContext context;
+
+        public ValuesController( MilljasContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Card> Get()
+        {
+           return context.Card.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IEnumerable<Card> Get(int id)
+        {
+           return context.Card.Where(tId => tId.TellerId == id).ToList();
+        }
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
